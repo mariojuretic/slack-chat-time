2a2bdfc8ccd728523d9e5671b49f617f6fa6f6f3
mariojuretic <jureticm23@gmail.com>
2019-04-21 21:07:26 +0200

    Added configuration infrastructure

---
+++ config/index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+
+module.exports = {
+  locationiqApiKey: process.env.LOCATIONIQ_API_KEY,
+  timezonedbApiKey: process.env.TIMEZONEDB_API_KEY
+};
--- server/service.js
+++ server/service.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const config = require("../config");
 const express = require("express");
 const service = express();
 const request = require("superagent");
@@ -7,7 +8,7 @@ const moment = require("moment");
 
 service.get("/service/:location", (req, res, next) => {
   request.get(
-    `https://eu1.locationiq.com/v1/search.php?key=a6341429c31c09&q=${encodeURI(req.params.location)}&format=json`,
+    `https://eu1.locationiq.com/v1/search.php?key=${config.locationiqApiKey}&q=${encodeURI(req.params.location)}&format=json`,
     (err, response) => {
       if (err) {
         console.log(err);
@@ -20,7 +21,7 @@ service.get("/service/:location", (req, res, next) => {
       };
 
       request.get(
-        `http://api.timezonedb.com/v2.1/get-time-zone?key=UK33EMOT6PY3&format=json&by=position&lat=${location.lat}&lng=${location.lng}`,
+        `http://api.timezonedb.com/v2.1/get-time-zone?key=${config.timezonedbApiKey}&format=json&by=position&lat=${location.lat}&lng=${location.lng}`,
         (err, response) => {
           if (err) {
             console.log(err);
